41752289e3fbc9ff1f919f7e1fcbdc831b9aa92c
baidala <stavr@bk.ru>
2016-02-15 20:56:24 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,6 +6,8 @@
 package powerhibernate;
 
 import java.io.File;
+import java.util.ArrayList;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -27,21 +29,64 @@ public class Main {
         
         factory = HibernateUtil.getSessionFactory();
                 
-        addStudent( new Student ("Viktor", 5) );
+        //addStudent( new Student ("Viktor", 25) );
+        listStudent();
         
         
         
         
     }
 
-    private static void addStudent(Student student) {
+   // private static void addStudent(Student student) {
+    //    Session session = factory.openSession();
+    //    Transaction tr = session.beginTransaction();
+        
+     //   Integer insertID = (Integer) session.save(student);
+      //  tr.commit();
+       // session.close();
+        
+    //}
+    
+    private static Integer addStudent(Student student) {
         Session session = factory.openSession();
-        Transaction tr = session.beginTransaction();
-        
-        Integer insertID = (Integer) session.save(student);
-        tr.commit();
-        session.close();
-        
+        Transaction tr = null;
+        Integer studentID = null;
+        try {
+            tr = session.beginTransaction();
+            studentID = (Integer) session.save(student);
+            tr.commit();
+        } catch (HibernateException ex) {
+            if ( tr != null ) {
+            tr.rollback();
+            }
+        }
+        finally {
+            session.close();
+        }
+        return studentID;
+    }
+    
+    
+    
+    
+    public static void listStudent() {
+        Session session = factory.openSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            ArrayList<Student> listStudent = (ArrayList<Student>) session.createQuery("FROM Student").list();
+            for (int i = 0; i < listStudent.size(); i++) {
+                System.out.println(listStudent.get(i)); 
+            }
+            tx.commit();
+        } catch (HibernateException ex) {
+            if ( tx != null ) {
+                tx.rollback();
+            }
+                    
+                    
+        }
+       
     }
     
 }
